abd51e2ab5409065144f9a12e0430e94fe736a01
Yousei <72794107+yousei-github@users.noreply.github.com>
2023-01-16 13:31:26 +0800

    Delete Codes_C directory

--- Codes_C/src/DataType.c
+++
@@ -1,9 +0,0 @@
-#include "DataType.h"
-#include "Configuration.h"
-
-// remember to free the allocated memory
-void benchmark_deallocation(BenchmarkType* _benchmark)
-{
-    free(_benchmark->memorytrace);
-    free(_benchmark->total_page);
-}
