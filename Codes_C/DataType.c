04b443b1d285e988b32f29519c5b9e87b63fd9a2
Yousei <72794107+yousei-github@users.noreply.github.com>
2023-01-16 13:31:11 +0800

    Delete DataType.c

--- Codes_C/DataType.c
+++
@@ -1,8 +0,0 @@
-#include "DataType.h"
-
-// remember to free the allocated memory
-void benchmark_deallocation(BenchmarkType *_benchmark)
-{
-    free(_benchmark->memorytrace); 
-    free(_benchmark->totalpage); 
-}
